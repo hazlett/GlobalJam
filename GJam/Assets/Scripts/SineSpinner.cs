0bd2d43af169076a42b5c84fa045e1737e10f2ca
imaekgames <imaekgames@gmail.com>
2015-01-24 09:31:05 -0500

    Rotate Script

---
+++ GJam/Assets/Scripts/SineSpinner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineSpinner : MonoBehaviour {
+	float speed = 0;
+
+	void Update () {
+		speed = Mathf.MoveTowards(5,200,Time.deltaTime * 5);
+		transform.Rotate(0,0, speed);
+	}
+}
