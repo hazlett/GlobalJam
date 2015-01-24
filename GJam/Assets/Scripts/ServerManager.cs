3b52fcfd3f98c529f717a41326388397fb649fa7
hazlett <hazlett.206@gmail.com>
2015-01-24 00:20:08 -0500

    basic server

---
+++ GJam/Assets/Scripts/ServerManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerManager : MonoBehaviour {
+    private string code;
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StartCoroutine("SendCode");
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            StartCoroutine("ClearSession");
+        }
+    }
+
+
+    internal IEnumerator SendCode()
+    {
+        WWW www = new WWW("http://hazlett206.ddns.net/GlobalJam/SendPlayerCodes.php");
+        yield return www;
+        if (www.error == null)
+        {
+            Debug.Log(www.text);
+            code = www.text;
+        }
+        else
+        {
+            Debug.Log("error" + www.error);
+        }
+    }
+
+    internal IEnumerator ClearSession()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("code", code);
+        WWW www = new WWW("http://hazlett206.ddns.net/GlobalJam/ClearSession.php", form);
+        yield return www;
+    }
+}
