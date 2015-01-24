830cce83cd5905ea5072c74a4e8a9527e0061089
imaekgames <imaekgames@gmail.com>
2015-01-24 17:05:55 -0500

    Final Commit :(
    Boo hoo.

--- GJam/Assets/Scripts/SineSpinner.cs
+++ GJam/Assets/Scripts/SineSpinner.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class SineSpinner : MonoBehaviour {
-	float speed = 0;
+
 
 	void Update () {
-		speed = Mathf.MoveTowards(5,200,Time.deltaTime * 5);
-		transform.Rotate(0,0, speed);
+
+		transform.Rotate(0,0, Mathf.Sin(Time.deltaTime * 5000.5f));
 	}
 }
