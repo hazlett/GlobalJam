740869c846dfcb4666008591d90f0e4fb9cd8c74
imaekgames <imaekgames@gmail.com>
2015-01-24 00:08:09 -0500

    CompanionApp Begin

---
+++ GJam/Assets/RotateSpinner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSpinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,0,-5);
+	}
+}
